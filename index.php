2d8ba2244ff924ffd2e0ed86787bef868f1d57c1
Glizzel Ann Toledo <31942110+glizzelann@users.noreply.github.com>
2018-11-09 10:30:41 +0800

    Updated index

--- index.php
+++ index.php
@@ -1,18 +1,4 @@
 <?php
-require_once("src/controller/LoginController.php");
-require_once("src/controller/DashboardController.php");
-
-$login = new LoginController();
-$dashboard = new DashboardController();
-
-if(isset($_POST['login'])){
-	$login->login();
-}
-else if(isset($_POST['export'])){
-	$dashboard->export_To_CSV();
-}
-else{
-	$login->on_Load();
-}
-
+	//PHP INI ERROR LOGGING
+	echo "Hello World!";
 ?>
